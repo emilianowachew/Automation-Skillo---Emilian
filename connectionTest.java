21b9b4da9dd0a315678b8642b3ff6eec6b46c3a6
emilianowachew <emilian.novachev@abv.bg>
2024-01-30 14:53:19 +0200

    I am testing the connection
    Test

---
+++ connectionTest.java
@@ -0,0 +1,12 @@
+public class connectionTest {
+
+    String firstName = "Emilian";
+    String lastName = "Novachev";
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    System.out.println(firstName + lastName);
+}
+//connection successfully done//
