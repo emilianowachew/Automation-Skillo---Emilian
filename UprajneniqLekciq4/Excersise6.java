603274ceb033699e9502cff1f5181937ff099782
emilianowachew <emilian.novachev@abv.bg>
2024-02-03 11:41:09 +0200

    Create Excersise 6

---
+++ UprajneniqLekciq4/Excersise6.java
@@ -0,0 +1,8 @@
+package UprajneniqLekciq4;
+public class Excersise6 {
+    public static void main(String[] args) {
+        int arr[] = {12, 15, 32, 42, 55, 75, 122, 132, 150, 180, 200};
+        for(int i=0;i<=150;i++){
+            System.out.println(arr[i]);
+        }
+    }
